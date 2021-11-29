4641a86b805ca9eb52c7fc7376b4fd33f7428dfb
Gerardo Manuel Ascencio <gascencio@college.harvard.edu>
2021-11-29 15:09:21 -0500

    password input skeleton (has a bug)

--- popup.js
+++ popup.js
@@ -1,14 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var checkPageButton = document.getElementById('clickit');
-  checkPageButton.addEventListener('click', function () {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      alert(tabs[0].url);
-    })
-    // chrome.tabs.query({
-    //   active: true,
-    //   lastFocusedWindow: true
-    // }, function (tabs) {
-    //   alert(tab.url);
-    // })
-  })
+  // var checkPageButton = document.getElementById('clickit');
+  // checkPageButton.addEventListener('click', function () {
+  //   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  //     alert(tabs[0].url);
+  //   })
+  // })
+
+  function logSubmit(event) {
+    // log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
+    // event.preventDefault();
+    console.log(password)
+  }
+
+  const form = document.getElementById('password_form');
+  const password = document.getElementById('password_value');
+  form.addEventListener('submit', logSubmit);
 })
