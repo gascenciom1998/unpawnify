faed3f276b1c7651f2776cb0e5f7a7e8013168e0
Jack Boettcher <jcboettcher@college.harvard.edu>
2021-12-08 15:54:02 -0500

    added more features

--- content.js
+++ content.js
@@ -1,8 +1,58 @@
+const randomNonce = function(length) {
+  let text = "";
+  const possible = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm0123456789";
+  for(let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.msg === "password_value") {
-      password = request.password;
-      $("input[type='password']").val(password);
+    switch (request.msg) {
+      case "fill_all": {
+        password = request.password;
+        const inputs = $("input[type='password']")
+        inputs.val(password);
+        inputs.css("background","rgb(255 234 153)");
+        break;
+      }
+      case "hash_all": {
+        const inputs = $("input[type='password']");
+        const vals = [];
+        for (let input of inputs) {
+          vals.push(input.value);
+        }
+        sendResponse({class: "", values: vals});
+        break;
+      }
+      case "hash_selected": {
+        const input = document.activeElement;
+        if (input.tagName.toLowerCase() === "input" && input.type === "password") {
+          const vals = [input.value];
+          const className = randomNonce(20);
+          input.classList.add(className);
+          sendResponse({class: className, values: vals});
+        } else {
+          sendResponse({error: true});
+        }
+        break;
+      }
+      case "hashes": {
+        const inputs = request.class === ""
+          ? $("input[type='password']")
+          : $("."+request.class);
+        for (let i = 0; i < inputs.length; i++) {
+          const input = inputs.eq(i);
+          input.val(request.hashes[i]);
+          input.css("background","rgb(255 234 153)");
+          if (request.class !== "") {
+            input.removeClass(request.class);
+          }
+        }
+        break;
+      }
+      default: {}
     }
     return true;
   }
--- popup_bundled.js
+++ popup_bundled.js
@@ -521,30 +521,62 @@
 
 }).call(this)}).call(this,require('_process'),typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"_process":3}],2:[function(require,module,exports){
-// document.addEventListener('DOMContentLoaded', function () {
-//   }
-
 var sha256 = require('js-sha256').sha256;
 
-function passToContent(tabs) {
+function fillAllMsg(tabs) {
   const password = document.getElementById('password_value').value;
-  if(chrome.runtime.lastError) {
+  if (chrome.runtime.lastError) {
     alert("error");
-    setTimeout(passToContent, 1000);
+    setTimeout(() => fillAll(tabs), 1000);
   } else {
-    url = new URL(tabs[0].url)
-    console.log(url.hostname);
-    chrome.tabs.sendMessage(tabs[0].id, { msg: "password_value", password: sha256(url.hostname + password) }, function(response) {});
+    url = new URL(tabs[0].url);
+    chrome.tabs.sendMessage(tabs[0].id, { msg: "fill_all", password: sha256(url.hostname + password) }, function(response) {});
   }
   return true;
 }
 
-function logSubmit(event) {
-  chrome.tabs.query({ active: true, currentWindow: true }, passToContent);
+function hashMsg(tabs,msg) {
+  if (chrome.runtime.lastError) {
+    alert("error");
+    setTimeout(() => hashMsg(tabs,msg), 1000);
+  } else {
+    url = new URL(tabs[0].url);
+    chrome.tabs.sendMessage(tabs[0].id, { msg: msg }, function(response) {
+      if (response.error) {
+        document.getElementById("errorMsg").style.visibility = "visible";
+      } else {
+        chrome.tabs.sendMessage(tabs[0].id, { msg: "hashes", class: response.class,
+          hashes: response.values.map(val => sha256(url.hostname + val))
+        }, function(response) {});
+      }
+    });
+  }
+  return true;
+}
+
+function fillAll(event) {
+  document.getElementById("errorMsg").style.visibility = "hidden";
+  chrome.tabs.query({ active: true, currentWindow: true }, fillAllMsg);
+}
+
+function hashAll(event) {
+  document.getElementById("errorMsg").style.visibility = "hidden";
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => hashMsg(tabs,"hash_all"));
+}
+
+function hashSel(event) {
+  document.getElementById("errorMsg").style.visibility = "hidden";
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => hashMsg(tabs,"hash_selected"));
 }
 
 const form = document.getElementById('password_form');
-form.addEventListener('submit', logSubmit);
+form.addEventListener('submit', fillAll);
+
+const hashAllBtn = document.getElementById('hash_all_button');
+hashAllBtn.addEventListener('click', hashAll);
+
+const hashSelBtn = document.getElementById('hash_sel_button');
+hashSelBtn.addEventListener('click', hashSel);
 
 },{"js-sha256":1}],3:[function(require,module,exports){
 // shim for using process in browser
